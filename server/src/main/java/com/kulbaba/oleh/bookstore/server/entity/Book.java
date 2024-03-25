92c507a0e5ce9120a2dfd08c33febc95113383a5
BranetE <elatedexorcist@gmail.com>
2024-03-25 18:55:49 +0200

    Created Entity and Repository

--- server/src/main/java/com/kulbaba/oleh/bookstore/server/entity/Book.java
+++ server/src/main/java/com/kulbaba/oleh/bookstore/server/entity/Book.java
@@ -1,2 +1,31 @@
-package com.kulbaba.oleh.bookstore.server.entity;public class Book {
+package com.kulbaba.oleh.bookstore.server.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    private UUID id = UUID.randomUUID();
+    @Column
+    private String title;
+    @Column
+    private String isbn;
+    @Column
+    private Integer quantity;
 }
--- server/src/main/java/com/kulbaba/oleh/bookstore/server/repository/BookRepository.java
+++ server/src/main/java/com/kulbaba/oleh/bookstore/server/repository/BookRepository.java
@@ -1,2 +1,11 @@
-package com.kulbaba.oleh.bookstore.server.repository;public interface BookRepository {
+package com.kulbaba.oleh.bookstore.server.repository;
+
+import com.kulbaba.oleh.bookstore.server.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
 }
