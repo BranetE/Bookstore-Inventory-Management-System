19322f0104061977871bb388b52023793e223fd0
BranetE <elatedexorcist@gmail.com>
2024-03-29 17:32:18 +0200

    Fixed BookMapper

--- server/src/main/java/com/kulbaba/oleh/bookstore/server/mapper/BookMapper.java
+++ server/src/main/java/com/kulbaba/oleh/bookstore/server/mapper/BookMapper.java
@@ -1,29 +1,17 @@
 package com.kulbaba.oleh.bookstore.server.mapper;
 
 import com.kulbaba.oleh.bookstore.server.entity.Book;
-import kulbaba.oleh.bookstore.BookServiceOuterClass.BookInfoListResponse;
 import kulbaba.oleh.bookstore.BookServiceOuterClass.UpdateBookRequest;
 import kulbaba.oleh.bookstore.BookServiceOuterClass.BookInfoResponse;
 import kulbaba.oleh.bookstore.BookServiceOuterClass.CreateBookRequest;
 import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring", collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED)
 public interface BookMapper {
-    @Mapping(target = "id", expression = "java(UUID.randomUUID())")
     Book createBookRequestToBook(CreateBookRequest request);
     BookInfoResponse bookToBookInfoResponse(Book book);
     void updateBookFromUpdateBookRequest(@MappingTarget Book book, UpdateBookRequest request);
 
-    default BookInfoListResponse bookListToBookInfoListResponse(List<Book> bookList){
-        return BookInfoListResponse
-                .newBuilder()
-                .addAllBooks(bookList.stream().map(this::bookToBookInfoResponse).toList())
-                .build();
-    }
 }
