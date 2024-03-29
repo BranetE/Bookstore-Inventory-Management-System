70251e7817239163a9fff9b46d3c64102314607e
BranetE <elatedexorcist@gmail.com>
2024-03-29 17:31:35 +0200

    Provided Liquibase changelog

---
+++ server/src/main/resources/db/changelog/db.changelog-create-schema.sql
@@ -0,0 +1,14 @@
+-- liquibase formatted sql
+
+--changeset OlehKulbaba:1
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS books
+(
+    id uuid not null primary key default gen_random_uuid(),
+    title    varchar(255),
+    quantity integer,
+    isbn     varchar(255),
+    author varchar(255)
+);
+--rollback DROP TABLE books
