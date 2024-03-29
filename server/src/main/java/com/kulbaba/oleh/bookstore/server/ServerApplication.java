fd71638b18fd4ebcaa9b64f8d60b59829ff0b3ca
BranetE <elatedexorcist@gmail.com>
2024-03-29 17:32:43 +0200

    Added BookRepository

--- server/src/main/java/com/kulbaba/oleh/bookstore/server/ServerApplication.java
+++ server/src/main/java/com/kulbaba/oleh/bookstore/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.kulbaba.oleh.bookstore.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 public class ServerApplication {
 
     public static void main(String[] args) {
